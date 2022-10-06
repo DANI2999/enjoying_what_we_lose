e491f4507d609c3076d10899a6863c850a01fd04
DANGEROUS <110321607+DANI2999@users.noreply.github.com>
2022-10-06 23:33:28 +0400

    clock in cube style2

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
-setInterval(time_timer, 1000);
-
-function time_timer() {
-  var dt = new Date();
-
-  var s = dt.getSeconds();
-  var s_p = (s / 60) * 100 + "%";
-
-  var m = dt.getMinutes();
-  var m_p = (m / 60) * 100 + "%";
-
-  var h = dt.getHours();
-  var h_p = (h / 24) * 100 + "%";
-
-  $(".hour").find("p").text(h);
-  $(".minute").find("p").text(m);
-  $(".second").find("p").text(s);
-
-  TweenMax.to($(".second"), 0.5, {
-    ease: Bounce.easeOut,
-    width: s_p
-  });
-  TweenMax.to($(".minute"), 0.5, {
-    ease: Bounce.easeOut,
-    width: m_p
-  });
-  TweenMax.to($(".hour"), 0.5, {
-    ease: Bounce.easeOut,
-    width: h_p
-  });
+function date_time(id)
+{
+        date = new Date;
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        document.getElementById("s").innerHTML = ''+s;
+        document.getElementById("m").innerHTML = ''+m;
+        document.getElementById("h").innerHTML = ''+h;
+        setTimeout('date_time("'+"s"+'");','1000');
+        return true;
 }
+window.onload = date_time('s');
